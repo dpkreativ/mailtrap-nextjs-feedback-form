bb58ca81099761d7e570e0f9f963754c87621a8f
Divine Orji <dpkreativ@gmail.com>
2022-09-21 04:55:03 +0100

    Set up React Context - Created AppContext.js file with an instance of React Context - Imported AppContext into _app.js and set global state

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import AppContext from '../context/AppContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [feedbackData, setFeedbackData] = useState({
+    firstName: '',
+    lastName: '',
+    feedback: '',
+  });
+  const [submitSuccessful, setSubmitSuccessful] = useState(false);
+
+  return (
+    <AppContext.Provider
+      value={{
+        state: {
+          feedbackData: feedbackData,
+          submitSuccessful: submitSuccessful,
+        },
+        trigger: {
+          setFeedbackData: setFeedbackData,
+          setSubmitSuccessful: setSubmitSuccessful,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
